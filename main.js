0b08b73ff8a459a041a5dd5269c893f8ddafb949
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-04-18 02:37:15 +0900

    Component: Product added

--- main.js
+++ main.js
@@ -1,6 +1,56 @@
+Vue.component("product", {
+  template: `
+  <div class="card">
+    <img class="card__img" 
+         :src="variables[displayItem].img" 
+         :class="{'card__img--inactive': !variables[displayItem].hasStock}">
+    </img>
+    <div class="card__info">
+      <h1>{{ name }}</h1>
+      <p v-if="variables[displayItem].hasStock">In Stock</p>
+      <p v-else>Out of Stock</p>
+      <div class="color-selection">
+        <div v-for="(color, index) in colors" 
+             :style="{ backgroundColor: color }"
+             class="color-box"
+             @mouseover="changeItem(index)">
+        </div>
+      </div>
+    </div>
+  </div>
+  `,
+  data() {
+    return {
+      name: "Vue-Mastery Socks",
+      variables: [
+        {
+          img:
+            "https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg",
+          hasStock: true,
+          color: "#344861",
+        },
+        {
+          img:
+            "https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg",
+          hasStock: false,
+          color: "#339E68",
+        },
+      ],
+      displayItem: 0,
+    };
+  },
+  computed: {
+    colors() {
+      return this.variables.map((item) => item.color);
+    },
+  },
+  methods: {
+    changeItem(index) {
+      this.displayItem = index;
+    },
+  },
+});
+
 const app = new Vue({
   el: "#app",
-  data: {
-    test: "test",
-  },
 });
